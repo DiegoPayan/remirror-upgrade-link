45c0bc9b027d64baec430bd0329cddbb25325545
Diego Armando Payan Lopez <diegogmr1335@gmail.com>
2022-06-09 18:36:41 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import { Remirror, useRemirror } from "@remirror/react";
+import { LinkExtension } from "remirror/extensions";
+
+const App = () => {
+  const { manager } = useRemirror({
+    extensions: () => [new LinkExtension({ autoLink: true })],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Remirror manager={manager} />
     </div>
   );
-}
+};
 
 export default App;
